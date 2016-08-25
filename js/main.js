714e3c6c4462de013802fc87f15ceec9586cd88e
Sergey Timchenko <art-street93@mail.ru>
2016-08-25 12:56:56 +0300

    Refactor js and added elements and modifiers for header.

--- js/main.js
+++ js/main.js
@@ -1,68 +1,66 @@
 //Module for counters
 (function() {
-	var doc      = document,
-		minus1   = doc.getElementById("btnMinus1"),
-		minus2   = doc.getElementById("btnMinus2"),
-		plus1    = doc.getElementById("btnPlus1"),
-		plus2    = doc.getElementById("btnPlus2"),
-		counter1 = doc.getElementById("counter1"),
-		counter2 = doc.getElementById("counter2");
+	var $document        = document,
+	decrementButton1 = $document.getElementById("btnMinus1"),
+	decrementButton1 = $document.getElementById("btnMinus2"),
+	incrementButton1 = $document.getElementById("btnPlus1"),
+	incrementButton2 = $document.getElementById("btnPlus2"),
+	counter1         = $document.getElementById("counter1"),
+	counter2         = $document.getElementById("counter2");
 
-	function minus(e, counter) {
+	decrementButton1.onclick = function(e) {
+		decrement(e, counter1);
+	};
+
+	decrementButton2.onclick = function(e) {
+		decrement(e, counter2);
+	};
+
+	incrementButton1.onclick = function(e) {
+		increment(e, counter1);
+	};
+
+	incrementButton2.onclick = function(e) {
+		increment(e, counter2);
+	};
+
+	function decrement(e, counter) {
 		e.preventDefault();
-		var count = counter.innerHTML;
 
+		var count = counter.innerHTML;
 		if(count > 0) {
 			count--;
 		}
-
 		counter.innerHTML = count;
 	}
 
-	function plus(e, counter) {
+	function increment(e, counter) {
 		e.preventDefault();
+
 		var count = counter.innerHTML;
-
 		count++;
-
 		counter.innerHTML = count;
 	}
-
-	minus1.onclick = function(e) {
-		minus(e, counter1);
-	};
-
-	minus2.onclick = function(e) {
-		minus(e, counter2);
-	};
-
-	plus1.onclick = function(e) {
-		plus(e, counter1);
-	};
-
-	plus2.onclick = function(e) {
-		plus(e, counter2);
-	};
 }());
 
 //Module for menu
 (function() {
-	var doc        = document,
-		menuBtn    = doc.getElementById("menu"),
-		crossBtn   = doc.getElementById("cross"),
-		list       = doc.getElementsByClassName("navigation__list")[0];
+	var $document   = document,
+		menuButton  = $document.getElementById("menu"),
+		crossButton = $document.getElementById("cross"),
+		menuList    = $document.getElementsByClassName("navigation__list")[0];
 
-	menuBtn.onclick = function(e) {
+	menuButton.onclick = function(e) {
 		e.preventDefault();
 
-		menuBtn.style.display = "none";
-		list.style.top = "0";
+		menuButton.style.display = "none";
+		menuList.style.top = "0";
 	};
 
-	crossBtn.onclick = function(e) {
+	crossButton.onclick = function(e) {
 		e.preventDefault();
 
-		menuBtn.style.display = "block";
-		list.style.top = "-130px";
+		menuButton.style.display = "block";
+		menuList.style.top = "-130px";
 	};
 }());
